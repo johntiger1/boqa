8ee6c17884f87935806d6079dc9b4f609176d180
johntiger1 <john.person1@gmail.com>
2017-07-25 12:02:54 -0400

    threading can improve, however we do see an overhead of 12%

--- new_boqa/src/test/MultiThreadEx.java
+++ new_boqa/src/test/MultiThreadEx.java
@@ -2,6 +2,7 @@ package test;
 
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 //this will be a success if we can show threads doing different things (i.e. using their different args to start at
 //different places in an array)
@@ -18,10 +19,10 @@ public class MultiThreadEx {
         {
             //every 1000 elts, multiply by a 10 factor
             arr[i] = i*j;
-            if (i%1000 == 0)
-            {
-                j*=2;
-            }
+//            if (i%1000 == 0)
+//            {
+//                j*=2;
+//            }
         }
 
 
@@ -61,6 +62,11 @@ public class MultiThreadEx {
             int sum = 0;
             for (int i = startInd; i< endInd; i++)
             {
+                try{Thread.sleep(5);}
+                catch (InterruptedException ie)
+                {
+
+                }
                 sum += arr[i];
 
             }
@@ -85,24 +91,46 @@ public class MultiThreadEx {
 
     public static void main(String[]args)
     {
-        final int SIZE = 100000;
-        final int NUM_THREADS = 8;
+        final int SIZE = 1000;
+
         int []arr = new int[SIZE];
         populateArray(arr);
-        System.out.println(Arrays.toString(arr));
         MySummmer temp;
+        long start = System.nanoTime();
+        final int NUM_THREADS = 8;
+
+
+        //System.out.println(Arrays.toString(arr));
+
+
+        Thread  [] threads = new Thread[NUM_THREADS];
+
         int start_ind;
         int end;
         for (int i = 0; i < 8; i++){
+
             start_ind = i*(SIZE/NUM_THREADS);
             end = (i+1)*(SIZE/NUM_THREADS);
+
             temp = new MySummmer(i, start_ind ,end );
 
             temp.setData(arr);
+            threads[i] = new Thread(temp); //|point to the same thing"
+            threads[i].start();
 
-            new Thread(temp).start();
-
+        }int i;
+        for(i = 0; i < threads.length; i++) {
+            try{
+                threads[i].join();}
+            catch (InterruptedException ie){
+                System.out.println("oop:");
+            }
         }
+        System.out.println("took" + (System.nanoTime()-start));
+        temp = new MySummmer(10,0,arr.length);
+        temp.setData(arr);
+        temp.sumArray(arr);
+        System.out.println("took" + (System.nanoTime()-start));
        //MultiThreadEx me = new MultiThreadEx();
 
        // MySummmer m1 = new MySummmer()
